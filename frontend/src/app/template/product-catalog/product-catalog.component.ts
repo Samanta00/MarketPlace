3d4f7c972f7c31d8044bcc57680ec83e60a00452
Ellen Samanta <ellen.samanta@outlook.com>
2023-06-28 12:44:54 -0300

    feat: new images added

--- frontend/src/app/template/product-catalog/product-catalog.component.ts
+++ frontend/src/app/template/product-catalog/product-catalog.component.ts
@@ -30,9 +30,17 @@ export class ProductCatalogComponent {
   products: Product[] = [];
   filteredProducts: Product[] = [];
   selectedCategoryId: number | null = null;
+
+  //imagens para ser relacionadas por categoria
   imageCategoryMap: ImageCategoryMap = {
+    27: '../../../assets/produts/produtos-padaria.jpg',
+    28: '../../../assets/produts/carnes.jpg',
     29: '../../../assets/produts/friosLaticinios.jpg',
-  
+    30: '../../../assets/produts/bebidas.jpg',
+    31: '../../../assets/produts/limpeza.jpg',
+    32: '../../../assets/produts/higiene.jpg',
+    33: '../../../assets/produts/hortfrut.jpg',
+    34: '../../../assets/produts/petshop.jpg',
     // Adicione outras associações de ID da categoria para URL da imagem
   };
 
@@ -41,6 +49,7 @@ export class ProductCatalogComponent {
     this.getProducts();
   }
 
+  //lista as categorias
   listCategories() {
     this.productService.getCategories().subscribe((categories) => {
       this.categories = categories;
@@ -48,6 +57,7 @@ export class ProductCatalogComponent {
     });
   }
 
+  //lista os protudos
   getProducts() {
     this.productService.getProducts().subscribe((products) => {
       this.products = products.map((product) => ({
@@ -58,6 +68,7 @@ export class ProductCatalogComponent {
     });
   }
 
+  //filtro para encontrar produtos por categorias e enviar a imagem relacionada ao id de categoria
   filterProductsByCategory(categoryId: number) {
     this.selectedCategoryId = categoryId;
     this.filteredProducts = this.products.filter((product) => product.category_id === categoryId);
